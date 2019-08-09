57b0e573ad175d9716f82ad827d441a1356f48f2
Shivam Sohane <shivamsohane@qait.com>
2019-08-09 11:41:33 +0530

    done

---
+++ src/Practice/DataTypes.java
@@ -0,0 +1,40 @@
+package Practice;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		String a = "shivam";
+		String b="sohane";
+		String c = a.concat(b);
+		boolean d = a.equals(b);
+		int e = a.compareTo(b);
+		String f = a.substring(2);
+		String g = a.substring(2, 5);
+		String h = b.toUpperCase();
+		String i = c.toLowerCase();
+		boolean j = a.startsWith("sh");
+		boolean k = a.endsWith("am");
+		String l = a.replace("kkkkk", "ddd");
+		String m = a.replace("vam", "ss");
+		
+		
+		
+		
+		
+		
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(m);
+
+	}
+
+}
